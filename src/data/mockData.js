e30254f29ef2e080cf8bcbab7461c890c772881c
DieguinhoSoares <diegosoares@hotmail.fr>
2026-06-28 16:45:01 -0300

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,11 +1,11 @@
 import { ASSET_TYPES } from "../config/categories";
 
 export const mockAssets = [
-  { id: "1", placaOuTag: "ABC1D23", assetType: ASSET_TYPES.CAMINHAO, cell: "Logística", responsavel: "Noelio" },
-  { id: "2", placaOuTag: "XYZ9F88", assetType: ASSET_TYPES.VAN, cell: "Logística", responsavel: "Noelio" },
-  { id: "3", placaOuTag: "ESC-336-014", assetType: ASSET_TYPES.PESADO, cell: "Gestão de Ativos", responsavel: "Bianca" },
-  { id: "4", placaOuTag: "TRT-D6T-007", assetType: ASSET_TYPES.PESADO, cell: "Gestão de Ativos", responsavel: "Rudkeler" },
-  { id: "5", placaOuTag: "TQ-CONT-002", assetType: ASSET_TYPES.CONTAINER_TANQUE, cell: "Gestão de Ativos", responsavel: "João Pedro" },
+  { id: "1", placaOuTag: "ABC1D23", assetType: ASSET_TYPES.CAMINHAO, uf: "SP", cell: "Logística", responsavel: "Noelio" },
+  { id: "2", placaOuTag: "XYZ9F88", assetType: ASSET_TYPES.VAN, uf: "SP", cell: "Logística", responsavel: "Noelio" },
+  { id: "3", placaOuTag: "ESC-336-014", assetType: ASSET_TYPES.PESADO, uf: "SP", cell: "Gestão de Ativos", responsavel: "Bianca" },
+  { id: "4", placaOuTag: "TRT-D6T-007", assetType: ASSET_TYPES.PESADO, uf: "SP", cell: "Gestão de Ativos", responsavel: "Rudkeler" },
+  { id: "5", placaOuTag: "TQ-CONT-002", assetType: ASSET_TYPES.CONTAINER_TANQUE, uf: "SP", cell: "Gestão de Ativos", responsavel: "João Pedro" },
 ];
 
 const todayPlus = (days) => {
@@ -19,7 +19,6 @@ export const mockDocumentsByAssetId = {
     { categoryId: "crlv", year: 2026, validUntil: todayPlus(120), fileUrl: "#", fileName: "CRLV_ABC1D23_2026.pdf" },
     { categoryId: "ipva", year: 2026, validUntil: todayPlus(-5), fileUrl: "#", fileName: "IPVA_ABC1D23_2026.pdf" },
     { categoryId: "tacografo", year: 2025, validUntil: todayPlus(15), fileUrl: "#", fileName: "Tacografo_ABC1D23_2025.pdf" },
-    // ANTT/RNTRC, Licenciamento, Certificado de Inspeção, CIV, Seguro: faltantes de propósito
   ],
   "2": [
     { categoryId: "crlv", year: 2026, validUntil: todayPlus(200), fileUrl: "#", fileName: "CRLV_XYZ9F88_2026.pdf" },
@@ -36,9 +35,7 @@ export const mockDocumentsByAssetId = {
     { categoryId: "nf_aquisicao", year: 2022, validUntil: null, fileUrl: "#", fileName: "NF_ESC336014.pdf" },
     { categoryId: "ficha_tecnica", year: 2022, validUntil: null, fileUrl: "#", fileName: "FichaTecnica_ESC336014.pdf" },
   ],
-  "4": [
-    // tudo faltante de propósito (ativo novo, recém-cadastrado)
-  ],
+  "4": [],
   "5": [
     { categoryId: "cipp", year: 2025, validUntil: todayPlus(-10), fileUrl: "#", fileName: "CIPP_TQCONT002_2025.pdf" },
     { categoryId: "seguro_apolice", year: 2026, validUntil: todayPlus(300), fileUrl: "#", fileName: "Apolice_TQCONT002_2026.pdf" },
